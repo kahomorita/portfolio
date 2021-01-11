7656b7d68cd7ae865a0c8af8729c940570753159
kahomorita <kahomaru2679@yahoo.co.jp>
2021-01-11 20:13:36 +0900

    確認画面完成

--- confirm.php
+++ confirm.php
@@ -8,15 +8,23 @@
   <title>確認画面</title>
 </head>
 <body>
-  <h1>メッセージ内容</h1>
-  <form action="complete.php" method="post">
-    <div class="message_info">
-      <p>name:</p><?php echo htmlspecialchars($_POST['name']); ?>
-    </div>
-    <p>mail address:</p><?php echo htmlspecialchars($_POST['mail']); ?>
-    <p>message:</p><?php echo htmlspecialchars($_POST['message']); ?>
+  <div class="inner confirm">
+    <h1 class="message_confirm">メッセージ内容</h1>
+    <form action="complete.php" method="post">
+      <div class="message_info">
+        <p>name:</p><?php echo htmlspecialchars($_POST['name']); ?>
+      </div>
+      <div class="message_info">
+        <p>mail address:</p><?php echo htmlspecialchars($_POST['mail']); ?>
+      </div>
+      <div class="message_info">
+        <p>message:</p><?php echo htmlspecialchars($_POST['message']); ?>
+      </div>
 
-    <button type="submit">送信</button>
-  </form>
+      <p class="last_confirm">こちらの内容で送信してもよろしいですか？</p>
+
+      <button type="submit" class="confirm_button">OK</button>
+    </form>
+  </div>
 </body>
 </html>
