befa8f2d0386a7475da4f58615167af9ba701a3c
kahomorita <kahomaru2679@yahoo.co.jp>
2021-02-01 14:30:46 +0900

    phpmailer完了

--- confirm.php
+++ confirm.php
@@ -8,22 +8,63 @@ if (!isset($_SESSION['form'])) {
   $post = $_SESSION['form'];
 }
 
-if($_SERVER['REQUEST_METHOD']==='POST') {
-    $to = "kahomaru2679@yahoo.co.jp";
-    $from = $post['mail'];
-    $subject = 'メッセージが届きました';
-    $body = <<<EOT
-    名前： {$post['name']}
-    メールアドレス： {$post['mail']}
-    メッセージ内容：
-    {$post['message']}
-    EOT;
+// if($_SERVER['REQUEST_METHOD']==='POST') {
+//     $to = "kahomaru2679@yahoo.co.jp";
+//     $from = $post['mail'];
+//     $subject = 'メッセージが届きました';
+//     $body = <<<EOT
+//     名前： {$post['name']}
+//     メールアドレス： {$post['mail']}
+//     メッセージ内容：
+//     {$post['message']}
+//     EOT;
 
-    mb_send_mail($to,$subject,$body,"From:{$from}");
-    unset($_SESSION['form']);
-    header('Location:complete.html');
-    exit();
+//     mb_send_mail($to,$subject,$body,"From:{$from}");
+//     unset($_SESSION['form']);
+//     header('Location:complete.html');
+//     exit();
+// }
+
+$name    = $post['name'];
+$address      = $post['mail'];
+$message = $post['message'];
+
+mb_language("japanese");
+mb_internal_encoding("UTF-8");
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require ('vendor/autoload.php');
+
+$mail = new PHPMailer(true);
+
+try {
+  // SMTPの設定
+  $mail->isSMTP();                       // SMTP 利用
+  $mail->Host       = 'smtp.gmail.com';  // SMTP サーバー(Gmail の場合これ)
+  $mail->SMTPAuth   = true;              // SMTP認証を有効にする
+  $mail->Username   = 'kaho2679@gmail.com'; // ユーザ名 (Gmail ならメールアドレス)
+  $mail->Password   = '26790512';      // パスワード
+  $mail->SMTPSecure = 'ssl';             // 暗号化通信 (Gmail では使えます)
+  $mail->Port       = 465;               // TCP ポート (TLS の場合 587)
+
+  // メール本体
+  $mail->setFrom($address, mb_encode_mimeheader($name, 'ISO-2022-JP'));  // 送信元メールアドレスと名前
+  $mail->addAddress('kaho2679@gmail.com');  // 送信先メールアドレスと名前
+  $mail->Body    = mb_convert_encoding($message, "JIS","UTF-8");  // 本文
+
+// 送信
+  $mail->send();
+  echo '送信済み';
+  unset($_SESSION['form']);
+      header('Location: complete.html');
+      exit();
+} catch (Exception $e) {
+  echo "送信失敗: {$mail->ErrorInfo}";
 }
+
 
 ?>
 
